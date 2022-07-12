956f13625c9cf05bafe931c08a61796c1e6a6415
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-07-12 23:11:29 +0700

    add enhance for

--- src/Staff_Manager.java
+++ src/Staff_Manager.java
@@ -7,11 +7,11 @@ public class Staff_Manager {
         Staff[] employees = new Staff[8];
 
         employees[0] = new Fulltime(1, "Trong", 18, 984426498, "tronglu@yahoo.com", 70, 50, 1450);
-        employees[1] = new Fulltime(36, "Phuong", 19, 984426498, "phuong@yahoo.com", 100, 70,890 );
+        employees[1] = new Fulltime(36, "Phuong", 19, 984472498, "phuong@yahoo.com", 100, 70,890 );
         employees[2] = new Fulltime(43, "Hiep", 18, 984426498, "hiep@yahoo.com", 30, 50, 344);
         employees[3] = new Fulltime(43, "Dung", 17, 984426498, "dung@yahoo.com", 50, 50, 560);
 
-        employees[4] = new Parttime(69, "Hieu", 15, 984426498, "hieu@yahoo.com", 80);
+        employees[4] = new Parttime(69, "Hieu", 15, 981126498, "hieu@yahoo.com", 80);
         employees[5] = new Parttime(34, "Nam", 18, 984426498, "nam@yahoo.com", 80);
         employees[6] = new Parttime(77, "Hoang", 19, 984426498, "hoang@yahoo.com", 70);
         employees[7] = new Parttime(77, "Quang Anh", 19, 984426498, "quanganh@yahoo.com", 100);
@@ -29,54 +29,66 @@ public class Staff_Manager {
         double totalPart = totalWageParttime(employees);
         System.out.println("Tong so luong phai tra cho nhan vien ban thoi gian la: " + totalPart);
 
-        System.out.println(getAvg(employees));
+        belowAvg(employees);
     }
 
 
-
-    static double netWageParttime(Parttime employ) {
-        double wage = 0;
-        wage = employ.getWorkTime() * 5;
+    //Tinh luong part time
+    static double netWageParttime(Parttime employPart) {
+        double wage;
+        wage = employPart.getWorkTime() * 5;
         return wage;
     }
 
-
+    //Tinh luong full time
     static double netWageFulltime(Fulltime employFull) {
-        double net = 0;
+        double net;
         net = (employFull.getWage() + (employFull.getBonus() - employFull.getFines()));
         return net;
     }
 
-    static int totalWageParttime(Staff[] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] instanceof Parttime) {
-                sum += netWageParttime((Parttime) array[i]);
+    //Tong luong phai tra cua part time
+    static double totalWageParttime(Staff[] array) {
+        double sum = 0;
+        for (Staff employ : array) {
+            if (employ instanceof Parttime) {
+                sum += netWageParttime((Parttime) employ);
             }
         }
         return sum;
     }
 
-
-    static int getAvg(Staff[] array) {
-        int sum1 = 0;
-        int sum2 = 0;
-        int total = 0;
-        for (int i = 0; i < array.length; i++) {
-            if(array[i] instanceof Fulltime ) {
-                sum1 += netWageFulltime((Fulltime) array[i]);
-            }
-            else if (array[i] instanceof Parttime) {
-                sum2 += netWageParttime((Parttime) array[i]);
+    //Tinh luong trung binh
+    static double getAvgWage(Staff[] array) {
+        double sum1 = 0;
+        double sum2 = 0;
+        for (Staff employ : array) {
+            if (employ instanceof Fulltime) {
+                sum1 += netWageFulltime((Fulltime) employ);
+            } else if (employ instanceof Parttime) {
+                sum2 += netWageParttime((Parttime) employ);
             }
         }
-        total = sum1 + sum2;
+        double total = sum1 + sum2;
         return total / array.length;
     }
 
+    //Cac nhan vien duoi muc luong trung binh
     static void belowAvg(Staff[] array) {
-        
+
+        for (Staff employ : array) {
+            double avg = getAvgWage(array);
+            double wageFull = netWageFulltime((Fulltime) employ);
+            if (employ instanceof Fulltime) {
+                if (wageFull < avg) {
+                    System.out.print("Cac nhan vien duoi muc luong trung binh la: " + employ.getName());
+                }
+            }
+        }
+
     }
+
+
 
 
 
