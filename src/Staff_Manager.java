f7ea423e63354928a212e2d17554de7128fcca24
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-07-13 07:39:12 +0700

    done, fix all bug

--- src/Staff_Manager.java
+++ src/Staff_Manager.java
@@ -7,7 +7,7 @@ public class Staff_Manager {
         Staff[] employees = new Staff[8];
 
         employees[0] = new Fulltime(1, "Trong", 18, 984426498, "tronglu@yahoo.com", 70, 50, 1450);
-        employees[1] = new Fulltime(36, "Phuong", 19, 984472498, "phuong@yahoo.com", 100, 70,890 );
+        employees[1] = new Fulltime(36, "Phuong", 19, 984472498, "phuong@yahoo.com", 100, 70, 890);
         employees[2] = new Fulltime(43, "Hiep", 18, 984426498, "hiep@yahoo.com", 30, 50, 344);
         employees[3] = new Fulltime(43, "Dung", 17, 984426498, "dung@yahoo.com", 50, 50, 560);
 
@@ -17,7 +17,6 @@ public class Staff_Manager {
         employees[7] = new Parttime(77, "Quang Anh", 19, 984426498, "quanganh@yahoo.com", 100);
 
 
-
         double fullTime = netWageFulltime((Fulltime) employees[0]);
         System.out.println(fullTime);
 
@@ -25,11 +24,12 @@ public class Staff_Manager {
         System.out.println(partTime);
 
 
-
         double totalPart = totalWageParttime(employees);
         System.out.println("Tong so luong phai tra cho nhan vien ban thoi gian la: " + totalPart);
 
-        belowAvg(employees);
+        getBelowAvg(employees);
+
+        sortAscendingFull(employees);
     }
 
 
@@ -73,26 +73,46 @@ public class Staff_Manager {
         return total / array.length;
     }
 
-    //Cac nhan vien duoi muc luong trung binh
-    static void belowAvg(Staff[] array) {
 
+    //Cac nhan vien duoi muc luong trung binh
+    static void getBelowAvg(Staff[] array) {
+        System.out.println("Cac nhan vien duoi muc luong trung binh:");
         for (Staff employ : array) {
-            double avg = getAvgWage(array);
-            double wageFull = netWageFulltime((Fulltime) employ);
             if (employ instanceof Fulltime) {
-                if (wageFull < avg) {
-                    System.out.print("Cac nhan vien duoi muc luong trung binh la: " + employ.getName());
+                if (netWageFulltime((Fulltime)employ) < getAvgWage(array)) {
+                    System.out.println(employ.getName());
                 }
             }
         }
-
     }
 
+    //Sap xep thu tu
+    static void sortAscendingFull(Staff[] array) {
+        Staff temp;
+        System.out.println("Sap xep luong nhan vien Fulltime tu thap den cao: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] instanceof Fulltime && array[j] instanceof Fulltime ) {
+                    double salary1 = netWageFulltime((Fulltime) array[i]);
+                    double salary2 = netWageFulltime((Fulltime) array[j]);
+                    if (salary1 > salary2) {
+                        temp = array[j];
+                        array[j] = array[i];
+                        array[i] = temp;
+                    }
+                }
+            }
 
-
-
-
+        }
+        for (Staff employ : array) {
+            if (employ instanceof Fulltime) {
+                System.out.println(employ.getName());
+            }
+        }
+    }
 }
+
+
 
 
 
